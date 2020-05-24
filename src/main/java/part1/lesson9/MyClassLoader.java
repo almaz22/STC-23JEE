0b057291d6181e7aac27d927a6331453d4228404
almaz22 <kamalov91@mail.ru>
2020-05-24 15:44:49 +0300

    ДЗ_9 Исправил собственный ClassLoader

--- src/main/java/part1/lesson9/MyClassLoader.java
+++ src/main/java/part1/lesson9/MyClassLoader.java
@@ -10,10 +10,11 @@ import java.nio.file.Paths;
 public class MyClassLoader extends ClassLoader {
 
     String PATH = Main_01_09_01.SOME_CLASS_PATH;
+    String PACKAGE = Main_01_09_01.PACKAGE;
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        if ((PATH + "SomeClass").equals(name)) {
+        if ((PACKAGE + "." + "SomeClass").equals(name)) {
             return findClass(name);
         }
         return super.loadClass(name);
@@ -22,9 +23,9 @@ public class MyClassLoader extends ClassLoader {
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         System.out.println("SomeClass starts work: " + name);
-        if ((PATH + "SomeClass").equals(name)) {
+        if ((PACKAGE + "." + "SomeClass").equals(name)) {
             try {
-                byte[] bytes = Files.readAllBytes(Paths.get(name + ".class"));
+                byte[] bytes = Files.readAllBytes(Paths.get(PATH + "SomeClass.class"));
                 return defineClass(name, bytes, 0, bytes.length);
             } catch (IOException e) {
                 e.printStackTrace();
